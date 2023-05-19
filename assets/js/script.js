c4ea169a2cdf59d6ac52d9fa0d1b40bccc9fcc32
sherlyls <sherlyls862@gmail.com>
2023-05-19 17:53:46 +0700

    first-commit

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+var swiperStorynews = new Swiper('.slider-storynews', {
+    slidesPerView: "auto",
+    spaceBetween: 12,
+});
+
+var swiperTvstreaming = new Swiper('.grup-streaming', {
+    slidesPerView: "auto",
+    spaceBetween: 12,
+});
+
+var swiperRadio = new Swiper('.grup-radio', {
+    slidesPerView: "auto",
+    spaceBetween: 12,
+});
